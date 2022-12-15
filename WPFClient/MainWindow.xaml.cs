4bbd95c56e24a48a900981c8996b52ad3527d4aa
Vikram Jadhav <vikramjadhav1015@gmail.com>
2022-12-15 17:57:40 +0530

    Setup WPF Client

--- WPFClient/MainWindow.xaml.cs
+++ WPFClient/MainWindow.xaml.cs
@@ -1,28 +1,108 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Windows;
+using Microsoft.AspNetCore.SignalR.Client;
+using System;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace WPFClient
+namespace WPFClient;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    HubConnection connection;
+
+    //constructor
+    public MainWindow()
     {
-        public MainWindow()
+        InitializeComponent();
+
+        //setup the connection
+        connection = new HubConnectionBuilder()
+            .WithUrl("https://localhost:7266/chathub")
+            .WithAutomaticReconnect()
+            .Build();
+
+        //Reconnecting Event
+        //if connection issue happens
+        //execute when trying to reconnect the hub
+        connection.Reconnecting += (sender) =>
         {
-            InitializeComponent();
+            this.Dispatcher.Invoke(() =>
+            {
+                var newMessage = "Attempting to reconnect...";
+                messages.Items.Add(newMessage); //listbox messages
+            });
+
+            return Task.CompletedTask;
+        };
+
+        connection.Reconnected += (sender) =>
+        {
+            this.Dispatcher.Invoke(() =>
+            {
+                var newMessage = "Reconnected to the server";
+                messages.Items.Clear();
+                messages.Items.Add(newMessage);
+            });
+
+            return Task.CompletedTask;
+        };
+
+
+        connection.Closed += (sender) =>
+        {
+            this.Dispatcher.Invoke(() =>
+            {
+                var newMessage = "Connection Closed";
+                messages.Items.Add(newMessage);
+
+                openConnection.IsEnabled= true; //to enable open connection button
+                sendMessage.IsEnabled= false; // to disable send message button
+            });
+
+            return Task.CompletedTask;
+        };
+    }
+
+    private async void openConnection_Click(object sender, RoutedEventArgs e)
+    {
+        //when message arrived at Hub this message will receive the message
+        connection.On<string, string>("ReceiveMessage", (user, message) =>
+        {
+            this.Dispatcher.Invoke(() =>
+            {
+                var newMessage = $"{user}: {message}";
+                messages.Items.Add(newMessage);
+            });
+
+        });
+
+
+        try
+        {
+            await connection.StartAsync();
+            messages.Items.Add("Connection Started");
+            openConnection.IsEnabled= false;
+            sendMessage.IsEnabled= true;
+        }
+        catch(Exception ex)
+        {
+            messages.Items.Add(ex.Message);
+        }
+    }
+
+    private async void sendMessage_Click(object sender, RoutedEventArgs e)
+    {
+        try
+        {
+            await connection.InvokeAsync("SendMessage",
+                "WPF Client", messageInput.Text);
+        }
+        catch (Exception ex)
+        { 
+            messages.Items.Add(ex.Message);
         }
     }
 }
